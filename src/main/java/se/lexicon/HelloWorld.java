e1f988a9125c0596417c236e4a370e5e7b573952
Mehrdad Javan <mehrdad.javan@lexicon.se>
2025-03-04 10:04:04 +0100

    variables

--- src/main/java/se/lexicon/HelloWorld.java
+++ src/main/java/se/lexicon/HelloWorld.java
@@ -18,6 +18,29 @@ public class HelloWorld {
     public static void main(String[] args) {
         // sout + Tab
         System.out.println("Hello World!");
+
+        // Variables
+        // [data type] [variableName];
+        int speed;
+        speed = 100;
+
+        double salary = 50000.55;
+        char grade = 'A';
+        char specialChar = '\u2764'; // '❤'
+        boolean isActive = true;
+        boolean isReserved = false;
+
+        long balance = 5000000000000L;
+        double area = 20.55;
+        int setNumber = 100;
+        int studentId = 5000;
+
+
+
+
+
+        System.out.println("Speed: " + speed);
     }
 
+       // Reformat the Code: Ctrl + Alt + L
 }

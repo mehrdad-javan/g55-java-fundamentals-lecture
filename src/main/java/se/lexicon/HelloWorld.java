d9b7c53aa312a4b2f043a9ae63873026ae490fd0
Mehrdad Javan <mehrdad.javan@lexicon.se>
2025-03-04 10:53:11 +0100

    operators and String

--- src/main/java/se/lexicon/HelloWorld.java
+++ src/main/java/se/lexicon/HelloWorld.java
@@ -34,13 +34,82 @@ public class HelloWorld {
         double area = 20.55;
         int setNumber = 100;
         int studentId = 5000;
-
-
-
-
-
         System.out.println("Speed: " + speed);
+        System.out.println("-----------------");
+
+        int number1 = 10;
+        int number2 = 5;
+
+        int additionResult = number1 + number2;
+        System.out.println("Addition: " + additionResult);
+        int subtractionResult = number1 - number2;
+        System.out.println("Subtraction: " + subtractionResult);
+        // *   /  %
+        int division = number1 / number2; // returns the quotient
+        System.out.println("Division: " + division);
+        int modules = number1 % number2; // returns the remainder of the division
+        System.out.println("Modules: " + modules);
+
+        // Increment (++) and Decrement (--) operators
+        int counter = 0;
+        System.out.println(++counter); // Output : 1
+
+        int num1 = 500, num2 = 200, num3 = 10;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+
+        // (== , != , < , > , <=, >=)
+        boolean isEqual = num1 == num2; // false
+        System.out.println("Is Equal: " + isEqual);
+        boolean result = num1 > num2; // true
+        System.out.println("Is GraterThan: " + result);
+
+
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote");
+        }
+
+
+        int marks = 85;
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        } else if (marks >= 80) {
+            System.out.println("Grade B");
+        } else if (marks >= 70) {
+            System.out.println("Grade C");
+        } else if (marks >= 55 || marks <= 60) {
+            System.out.println("Grade FX");
+        } else {
+            System.out.println("Grade F");
+        }
+
+        int dayOfWeek = 10;
+        switch (dayOfWeek) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            default:
+                System.out.println("Invalid Day.");
+        }
+
+        String name = "Mehrdad";
+        String lastName = "Javan";
+        name = name.concat(" ").concat(lastName); // Mehrdad Javan
+        System.out.println(name); // Mehrdad
+
+
     }
 
-       // Reformat the Code: Ctrl + Alt + L
+    // Reformat the Code: Ctrl + Alt + L
 }
